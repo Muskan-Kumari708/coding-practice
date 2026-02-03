6f449a801bd320ba474e9cc5d5f17ed6de5f7071
Muskan Kumari <muskan91025kumari@gmail.com>
2026-02-03 20:58:25 +0530

    Add Arrays Files

---
+++ Arrays/Ascending_Array.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Ascending_Array {
+    public static void main(String[] args){
+        int [] ascending = {3,5,8,1,9};
+        for (int i=0;i<ascending.length-1;i++){
+            for (int j=0;j< ascending.length-1;j++){
+                if (ascending[j]>ascending[j+1]){
+                    int t = ascending[j+1];
+                    ascending[j+1]=ascending[j];
+                    ascending[j]=t;
+                }
+            }
+            
+
+        }
+        for (int i=0;i< ascending.length;i++){
+            System.out.print(ascending[i]+" ");
+        }
+    }
+}
